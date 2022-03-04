631e66b088aa3d1e09f4106ee49998634be95922
David Such <dsuch@reefwing.com.au>
2022-03-04 14:44:50 +1100

    Update testAndCalibrate.ino

--- examples/testAndCalibrate/testAndCalibrate.ino
+++ examples/testAndCalibrate/testAndCalibrate.ino
@@ -42,89 +42,90 @@ void setup() {
   while (!Serial);
 
   if (imu.connected()) {
-      results = imu.selfTest();
+    Serial.println("Gyroscope SELF TEST");
+    Serial.println("Place the Nano 33 BLE on a flat surface and don't move it.");
+    
+    results = imu.selfTest();
 
-      Serial.println("Gyroscope SELF TEST");
-      Serial.println("Place the Nano 33 BLE on a flat surface and don't move it.");
-      Serial.print("x-axis: "); 
-      Serial.print(results.gyrodx); 
-      Serial.println(" dps"); 
+    Serial.print("x-axis: "); 
+    Serial.print(results.gyrodx); 
+    Serial.println(" dps"); 
 
-      Serial.print("y-axis: "); 
-      Serial.print(results.gyrody); 
-      Serial.println(" dps"); 
+    Serial.print("y-axis: "); 
+    Serial.print(results.gyrody); 
+    Serial.println(" dps"); 
 
-      Serial.print("z-axis: "); 
-      Serial.print(results.gyrodz); 
-      Serial.println(" dps"); 
+    Serial.print("z-axis: "); 
+    Serial.print(results.gyrodz); 
+    Serial.println(" dps"); 
 
-      Serial.println("Gyroscope x, y and z axis expected range: 20 - 250 dps");
+    Serial.println("Gyroscope x, y and z axis expected range: 20 - 250 dps");
 
-      if (sensorInRange(results.gyrodx, results.gyrody, results.gyrodz, 20.0, 250.0)) {
-        Serial.println("GYROSCOPE PASSED SELF-TEST");
-      }
-      else {
-        Serial.println("GYROSCOPE FAILED SELF-TEST");
-      }
+    if (sensorInRange(results.gyrodx, results.gyrody, results.gyrodz, 20.0, 250.0)) {
+      Serial.println("GYROSCOPE PASSED SELF-TEST");
+    }
+    else {
+      Serial.println("GYROSCOPE FAILED SELF-TEST");
+    }
 
-      Serial.println("\nAccelerometer SELF TEST");
-      Serial.print("x-axis: "); 
-      Serial.print(results.accdx); 
-      Serial.println(" mg"); 
+    Serial.println("\nAccelerometer SELF TEST");
+    Serial.print("x-axis: "); 
+    Serial.print(results.accdx); 
+    Serial.println(" mg"); 
 
-      Serial.print("y-axis = "); 
-      Serial.print(results.accdy); 
-      Serial.println(" mg"); 
+    Serial.print("y-axis = "); 
+    Serial.print(results.accdy); 
+    Serial.println(" mg"); 
 
-      Serial.print("z-axis = "); 
-      Serial.print(results.accdz); 
-      Serial.println(" mg"); 
+    Serial.print("z-axis = "); 
+    Serial.print(results.accdz); 
+    Serial.println(" mg"); 
 
-      Serial.println("Accelerometer x, y, and z axis expected range: 60 - 1700 mg");
+    Serial.println("Accelerometer x, y, and z axis expected range: 60 - 1700 mg");
 
-      if (sensorInRange(results.accdx, results.accdy, results.accdz, 60.0, 1700.0)) {
-        Serial.println("ACCELEROMETER PASSED SELF-TEST");
-      }
-      else {
-        Serial.println("ACCELEROMETER FAILED SELF-TEST");
-      }
+    if (sensorInRange(results.accdx, results.accdy, results.accdz, 60.0, 1700.0)) {
+      Serial.println("ACCELEROMETER PASSED SELF-TEST");
+    }
+    else {
+      Serial.println("ACCELEROMETER FAILED SELF-TEST");
+    }
 
-      Serial.println("\nIMU CALIBRATION");
+    Serial.println("\nIMU CALIBRATION");
 
-      // Calibrate gyroscope and accelerometer. Load offset biases into the bias registers
-      imu.calibrateAccGyro();
+    // Calibrate gyroscope and accelerometer. Load offset biases into the bias registers
+    imu.calibrateAccGyro();
 
-      Serial.println("\nACCELEROMETER & GYROSCOPE CALIBRATION COMPLETE.");
+    Serial.println("\nACCELEROMETER & GYROSCOPE CALIBRATION COMPLETE.");
 
-      Serial.println("\nMAGNETOMETER CALIBRATION:");
-      Serial.println("Wave the Nano 33 BLE in a vertical figure of eight until calibration is complete.");
-      imu.calibrateMag();
-      Serial.println("\nMAGNETOMETER CALIBRATION COMPLETE.");
+    Serial.println("\nMAGNETOMETER CALIBRATION:");
+    Serial.println("Wave the Nano 33 BLE in a vertical figure of eight until calibration is complete.");
+    imu.calibrateMag();
+    Serial.println("\nMAGNETOMETER CALIBRATION COMPLETE.");
 
-      biasOffsets = imu.getBiasOffsets();
+    biasOffsets = imu.getBiasOffsets();
 
-      Serial.println("\nAccelerometer Offset Bias (mg):"); 
-      Serial.println(1000.0 * biasOffsets.accelBias[0]); 
-      Serial.println(1000.0 * biasOffsets.accelBias[1]); 
-      Serial.println(1000.0 * biasOffsets.accelBias[2]);
+    Serial.println("\nAccelerometer Offset Bias (mg):"); 
+    Serial.println(1000.0 * biasOffsets.accelBias[0]); 
+    Serial.println(1000.0 * biasOffsets.accelBias[1]); 
+    Serial.println(1000.0 * biasOffsets.accelBias[2]);
 
-      Serial.println("\nGyroscope Offset Bias (dps):"); 
-      Serial.println(biasOffsets.gyroBias[0]); 
-      Serial.println(biasOffsets.gyroBias[1]); 
-      Serial.println(biasOffsets.gyroBias[2]);
+    Serial.println("\nGyroscope Offset Bias (dps):"); 
+    Serial.println(biasOffsets.gyroBias[0]); 
+    Serial.println(biasOffsets.gyroBias[1]); 
+    Serial.println(biasOffsets.gyroBias[2]);
 
-      Serial.println("\nMagnetometer Offset Bias (mG):"); 
-      Serial.println(1000.0 * biasOffsets.magBias[0]); 
-      Serial.println(1000.0 * biasOffsets.magBias[1]); 
-      Serial.println(1000.0 * biasOffsets.magBias[2]); 
+    Serial.println("\nMagnetometer Offset Bias (mG):"); 
+    Serial.println(1000.0 * biasOffsets.magBias[0]); 
+    Serial.println(1000.0 * biasOffsets.magBias[1]); 
+    Serial.println(1000.0 * biasOffsets.magBias[2]); 
 
-      Serial.println("\nLSM9DS1 IMU CALIBRATION COMPLETE.");
+    Serial.println("\nLSM9DS1 IMU CALIBRATION COMPLETE.");
 
-      Serial.println("\n\n  Code to copy and paste into your sketch:\n");
+    Serial.println("\n\n  Code to copy and paste into your sketch:\n");
 
-      printCode("\timu.loadAccBias", biasOffsets.accelBias);
-      printCode("\timu.loadGyroBias", biasOffsets.gyroBias);
-      printCode("\timu.loadMagBias", biasOffsets.magBias);
+    printCode("\timu.loadAccBias", biasOffsets.accelBias);
+    printCode("\timu.loadGyroBias", biasOffsets.gyroBias);
+    printCode("\timu.loadMagBias", biasOffsets.magBias);
   }
   else {
     Serial.println("LSM9DS1 IMU not found.");
