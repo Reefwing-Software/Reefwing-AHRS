45396ffeea85cdbaf788db6f8aca7c94b8ec38ac
David Such <dsuch@reefwing.com.au>
2022-03-09 11:59:34 +1100

    Update serialRollPitchYaw.ino

--- examples/serialRollPitchYaw/serialRollPitchYaw.ino
+++ examples/serialRollPitchYaw/serialRollPitchYaw.ino
@@ -16,7 +16,7 @@
 
 LSM9DS1 imu;
 EulerAngles angles;
-SensorData rawData;
+SensorData data;
 
 int loopFrequency = 0;
 const long displayPeriod = 1000;
@@ -60,15 +60,18 @@ void loop() {
 
   //  Display sensor data every displayPeriod, non-blocking.
   if (millis() - previousMillis >= displayPeriod) {
-    rawData = imu.rawData();
+    data = imu.rawData();
 
-    Serial.print("Raw ax, ay, az: ");
-    Serial.print(rawData.ax, rawData.ay, rawData.az);
-    Serial.print("  gx, gy, gz: ");
-    Serial.print(rawData.gx, rawData.gy, rawData.gz);
-    Serial.print("  mx, my, mz: ");
-    Serial.println(rawData.mx, rawData.my, rawData.mz);
-
+    Serial.print("ax = "); Serial.print(1000*data.ax);  
+    Serial.print(" ay = "); Serial.print(1000*data.ay); 
+    Serial.print(" az = "); Serial.print(1000*data.az); Serial.println(" mg");
+    Serial.print("gx = "); Serial.print( data.gx, 2); 
+    Serial.print(" gy = "); Serial.print( data.gy, 2); 
+    Serial.print(" gz = "); Serial.print( data.gz, 2); Serial.println(" deg/s");
+    Serial.print("mx = "); Serial.print(1000*data.mx ); 
+    Serial.print(" my = "); Serial.print(1000*data.my ); 
+    Serial.print(" mz = "); Serial.print(1000*data.mz ); Serial.println(" mG");
+      
     Serial.print("\nRoll:\t");
     Serial.print(angles.roll);
     Serial.print(" Pitch:\t");
